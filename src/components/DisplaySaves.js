b9f2fd5810f29ad5fef0fdb4238cd5b285666480
Roblouisck <robert@eddiebernard.com>
2019-08-08 20:31:05 -0700

    Setup for Netlify hosting
    Switched client id to env files; made redirects file for react-router to work with Netlify

--- src/components/DisplaySaves.js
+++ src/components/DisplaySaves.js
@@ -3,12 +3,11 @@ import { connect } from 'react-redux';
 import OrganizeSaves from '../containers/OrganizeSaves'
 import LoadingScreen from './LoadingScreen'
 import RenderHeader from './RenderHeader'
-import { CLIENT_ID } from '../keys';
 
 class DisplaySaves extends React.Component {
   componentDidMount = () => {
     if (this.props.token === null) {
-      window.location.replace(`https://www.reddit.com/api/v1/authorize?client_id=${CLIENT_ID}&response_type=token&state=123abc&redirect_uri=http://localhost:3000/authorize_callback&duration=temporary&scope=history identity`);
+      window.location.replace(`https://www.reddit.com/api/v1/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&state=123abc&redirect_uri=${process.env.REACT_APP_URI}&duration=temporary&scope=history identity`);
     }
     return null
   }
